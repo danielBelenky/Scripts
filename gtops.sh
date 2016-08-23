44ca92ce1b945cf97e0f0c63d231c1d2ebeff5aa
Daniel <daniel.belenky@gmail.com>
2016-08-23 22:01:50 +0300

    Some small updates with 'real' debug, and TODO for tomorrow

--- gtops.sh
+++ gtops.sh
@@ -3,7 +3,13 @@
 usage(){
 	cat<<EOF
 Usage:
+./daniel.sh <command to wrap> -arg <num of args to pass the wrapped command> <arg1> .. <arg2> .. <argN> <main args (-c --failed-count and etc...)>
+
+Example: ./daniel.sh ping -arg 1 www.google.com -c 3 --net-trace --debug
+
+        -h              Help
 	-c		Number of times to run the given command
+        
 	--failed-count	Number of allowed failed command invocation attempts before timing out
 	--net-trace		For each failed execution, create a 'pcap' file with the network traffic during the execution
 	--sys-trace		For each failed execution, create a log for each of the following values, measured during command execution:
@@ -14,6 +20,8 @@ Usage:
 EOF
 }
 
+_DEBUG="off"
+
 #Base vars
 COUNT=0
 FAILEDCNT=0
@@ -23,12 +31,11 @@ CALL_TRACE=false
 LOG_TRACE=false
 DEBUG=false
 
-#Command
+#Command and its args
 COM=$1
 shift
-
 #Set TENO wuth getiot parameters
-ARGS=`getopt -o c: --long failed-count:,net-trace,sys-trace,call-trace,log-trace,debug -- "$@"`
+ARGS=`getopt -o c:h --long failed-count:,net-trace,sys-trace,call-trace,log-trace,debug -- "$@"`
 eval set -- "$ARGS"
 # extract options and their arguments into variables.
 while true ; do
@@ -43,6 +50,11 @@ while true ; do
 		fi
 		
 		shift 2
+		;;
+
+	-h)
+		usage
+		break
 		;;
 
 	--failed-count)
@@ -71,6 +83,7 @@ while true ; do
 		;;
 
 	--debug)
+	        set -x
 		DEBUG=true
 		shift
 		;;
@@ -79,24 +92,16 @@ while true ; do
 		shift
 		break
 		;;
+
 	esac
 done
 
-if "$DEBUG"; then
-    echo "Arguments: "
-	echo -e "COUNT:\t\t"$COUNT
-	echo -e "FAILED COUNT:\t"$FAILEDCNT
-	echo -e "NET TRACE:\t"$NET_TRACE
-	echo -e "SYS TRACE:\t"$SYS_TRACE
-	echo -e "CALL TRACE:\t"$CALL_TRACE
-	echo -e "LOG TRACE:\t"$LOG_TRACE
-	echo -e "DEBUG:\t\t"$DEBUG
-fi
-
 #Execute the given command C times
+eCOM=eval "$COM"
+exStat=0 #Var to hold the exit status
 
 for (( i=1; i<=$COUNT; i++ )){
-	if $DEBUG; then
-	    echo "[DEBUG] $i outOf $COUNT. Exec: $COM"
-	fi
-}
+	#TODO:
+	#Add Commands hold functionality
+	#Check if the command is running (Maybe with ps?), and get it's exit status into an array.
+ }
